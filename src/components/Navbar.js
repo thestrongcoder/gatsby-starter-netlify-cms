9ed96be0e4377e940a70ed0a7bcf367a543d2700
thestrongcoder <stephleehales@gmail.com>
2021-11-01 22:22:02 +1000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -76,7 +76,7 @@ const Navbar = class extends React.Component {
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form
+                
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
